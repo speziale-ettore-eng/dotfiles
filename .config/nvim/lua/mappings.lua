28abbeb06fd6345636c44d763bee2f6bc89e3ab4
Ettore Speziale <speziale.ettore@gmail.com>
2026-07-12 18:40:14 -0700

    Bind `fm` in visual mode to format ...
    ... highlighted lines.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,3 +8,13 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Format visual selection using native LSP
+map("v", "<leader>fm", function()
+  vim.lsp.buf.format({
+    range = {
+      ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
+      ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
+    }
+  })
+end, { desc = "LSP format selected range" })
